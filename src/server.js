d4081c57fd099ac6b4c8b59d4332e14f8db7f45b
ngduyan201 <ngduyan201@gmail.com>
2024-12-26 23:04:47 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://ochuvn.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
